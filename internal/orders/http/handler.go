0477f66c9ddfa0723be728312d3cb96874b26988
Faraz Vahedi <kfv@kfv.io>
2025-09-10 23:34:53 +0330

    [order] Add http handlers
    Signed-off-by: Faraz Vahedi <kfv@kfv.io>

---
+++ internal/orders/http/handler.go
@@ -0,0 +1,81 @@
+package http
+
+import (
+	"dunhayat-api/internal/orders"
+	"dunhayat-api/internal/orders/usecase"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type OrderHandler struct {
+	createOrderUseCase usecase.CreateOrderUseCase
+}
+
+func NewOrderHandler(
+	createOrderUseCase usecase.CreateOrderUseCase,
+) *OrderHandler {
+	return &OrderHandler{
+		createOrderUseCase: createOrderUseCase,
+	}
+}
+
+func (h *OrderHandler) CreateOrder(c *fiber.Ctx) error {
+	var req orders.CreateOrderRequest
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid request body: " + err.Error(),
+		})
+	}
+
+	order, err := h.createOrderUseCase.Execute(c.Context(), &req)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(
+			fiber.Map{
+				"error": err.Error(),
+			},
+		)
+	}
+
+	response := fiber.Map{
+		"message": "Order created successfully",
+		"data":    order,
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(response)
+}
+
+func (h *OrderHandler) GetOrder(c *fiber.Ctx) error {
+	orderID := c.Params("id")
+	if orderID == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Order ID is required",
+		})
+	}
+
+	// TODO: Implement GetOrderUseCase
+
+	response := fiber.Map{
+		"message":  "Get order endpoint - to be implemented",
+		"order_id": orderID,
+	}
+
+	return c.Status(fiber.StatusOK).JSON(response)
+}
+
+func (h *OrderHandler) CancelOrder(c *fiber.Ctx) error {
+	orderID := c.Params("id")
+	if orderID == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Order ID is required",
+		})
+	}
+
+	// TODO: Implement CancelOrderUseCase
+
+	response := fiber.Map{
+		"message":  "Cancel order endpoint - to be implemented",
+		"order_id": orderID,
+	}
+
+	return c.Status(fiber.StatusOK).JSON(response)
+}
